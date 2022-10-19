22a8027df48fc127ad8f7cb05ad1dad03c962911
Alexei <97959334+Kalepson@users.noreply.github.com>
2022-10-19 18:10:58 +0300

    Centry

---
+++ javaScript.js
@@ -0,0 +1,46 @@
+
+const text = "значение переменной изменить нельзя.";
+let text_1 = "это способ объявить локальную переменную, значение которой может изменяться;";
+var text_2 = "это способ объявить глобальную переменную, значение которой может изменяться;";
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const string ="Salut";
+// const  number = 10 ;
+// const boolean = true;
+// const null_ = null;
+// let object = {
+// }
+// console.log(typeof object);
+// prompt()
+// console.log(number);
+
+// string = "nou text"
+// console.log(string);
+
+// Типы переменных
+// number
+// string
+// boolean
+// null
+// undefined
+// BigInt
+// symbol
+
+// object
